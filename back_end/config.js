1f8194c9e54a3d77e120fdc7273176317cf225c9
Jerick Cheong <jerickcheong@gmail.com>
2021-11-04 21:49:48 +0800

    fixed port

--- back_end/config.js
+++ back_end/config.js
@@ -2,4 +2,5 @@ const dotenv = require('dotenv');
 dotenv.config();
 module.exports = {
     DATABASE_URL: process.env.DATABASE_URL,
+    PORT: process.env.PORT,
 };
--- back_end/index.js
+++ back_end/index.js
@@ -1,6 +1,7 @@
 var app = require('./controller/app.js');
+let config = require('./config.js')
 var port = 8081;
 
-var server = app.listen(port, function () {
-    console.log("App hosted at localhost:" + port);
+var server = app.listen(config.PORT || 5000, function () {
+    console.log("App online");
 });
