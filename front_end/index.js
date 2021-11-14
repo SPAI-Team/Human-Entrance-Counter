05b2cdbd184704585089f3d722c1add99554f783
kiritowu <70313929+kiritowu@users.noreply.github.com>
2021-11-14 08:45:21 +0800

    Deploy front-end

--- front_end/index.js
+++ front_end/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
-app.use(express.static(__dirname + '/front_end'));
+app.use(express.static(__dirname ));
 
 app.listen(process.env.PORT || 5000, () => {
-    console.log(`Client server has started listening on port ${process.env.PORT}`);
+    console.log(`Client server has started listening on port ${process.env.PORT || 5000}`);
 });
