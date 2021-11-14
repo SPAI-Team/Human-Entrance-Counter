7ad9068290a4a83f55002e2d25f057cd6c1352b3
Amirrahman19 <48649124+Amirrahman19@users.noreply.github.com>
2021-11-14 10:29:26 +0800

    Update log.py

--- human_counter/custom_nodes/tracking/log.py
+++ human_counter/custom_nodes/tracking/log.py
@@ -1,81 +1,50 @@
-import datetime.datetime
-import requests
 import time
-import os
-import sys
-import logging
-import traceback
-from typing import Optional, Dict
+import requests
+from datetime import datetime
+from typing import Optional, Dict, Any
+from peekingduck.pipeline.nodes.node import AbstractNode
 
-from colorama import init, Fore, Style
+class Node(AbstractNode):
+    """<WHAT THIS NODE DOES>"""
+    def __init__(self, postIntervalTiming:int="5", endpoint:str="", config: Dict[str, Any] = None, **kwargs: Any) -> Dict[str, Any]:
+        self.__name__ = ''
 
+        self.endpoint = endpoint
+        self.postIntervalTiming = postIntervalTiming #>> NEED TO DO Type Casting FROM STRING TO DATETIME FOR COMPARISON PURPOSE
+        self.pastPostTiming = datetime.now()
 
-class LoggerSetup:  # pylint: disable=too-few-public-methods
-    """Set up the universal logging configuration"""
+        if config is None:
+            config = {
+                "input": ["footfall"],
+                "output": ["footfall"]7/ 
+            }
+        super().__init__(config, node_path=__name__, **kwargs)
+         
+    def run(self, inputs: Dict[str, Any]) -> Dict[str, Any]:
+        """Method that draws the count on the top left corner of image
+        
+         Args:
+             inputs (dict): Dict with keys "count".
+         Returns:
+             outputs (dict): None
+         """
+        current_footfall = inputs["footfall"]
+        current_time = datetime.now().strftime("%H:%M:%S")
+        
+        if current_time >= (self.pastPostTiming + self.pastPostTiming):
+            response = requests.post(
+                self.endpoint, 
+                data={
+                    "footfall": inputs['footfall']
+                }
+            )
+            ###### Check if the request has been posted successfully
+            ###### if request posted, 
+            ###### >> update the pastPostTiming
+            ###### >> update the footfall value ( current_footfall = 0)
 
-    def __init__(self) -> None:
-        if os.name == "nt":
-            init()
+            ###### if not posted, print out error message
+            
+            pastPostTiming = self.postIntervalTiming + current_time
 
-        formatter = ColoredFormatter(
-            "{asctime} {name} {level_color} {levelname}"
-            "{reset}: {msg_color} {message} {reset}",
-            style="{",
-            datefmt="%Y-%m-%d %H:%M:%S",
-            colors={
-                "DEBUG": Fore.RESET + Style.BRIGHT,
-                "INFO": Fore.RESET + Style.BRIGHT,
-                "WARNING": Fore.YELLOW + Style.BRIGHT,
-                "ERROR": Fore.RED + Style.BRIGHT,
-                "CRITICAL": Fore.RED + Style.BRIGHT,
-            },
-        )
-
-        handler = logging.StreamHandler(sys.stdout)
-        handler.setFormatter(formatter)
-
-        self.logger = logging.getLogger()
-        self.logger.handlers[:] = []
-        self.logger.addHandler(handler)
-        self.logger.setLevel(logging.INFO)
-        sys.excepthook = self.handle_exception
-
-    def handle_exception(  # type:ignore
-        self, exc_type, exc_value, exc_traceback
-    ) -> None:
-        """Use Python's logging module when showing errors"""
-
-        if issubclass(exc_type, KeyboardInterrupt):
-            sys.__excepthook__(exc_type, exc_value, exc_traceback)
-            return
-
-        error_list = traceback.format_exception(exc_type, exc_value, exc_traceback)
-        traceback_msg = " ".join([str(elem) for elem in error_list[:-1]])
-        error_msg = str(error_list[-1])
-
-        # Make the error type more obvious in terminal by separating these
-        self.logger.error(traceback_msg)
-        self.logger.error(error_msg)
-
-
-class ColoredFormatter(logging.Formatter):
-    """This class formats the color of logging messages"""
-
-    def __init__(
-        self, *args: str, colors: Optional[Dict[str, str]] = None, **kwargs: str
-    ) -> None:
-        """Initialize the formatter with specified format strings"""
-
-        super().__init__(*args, **kwargs)
-
-        self.colors = colors if colors else {}
-
-    def format(self, record: logging.LogRecord) -> str:
-        """Format the specified record as text."""
-
-        record.level_color = self.colors.get(record.levelname, "")
-        record.reset = Style.RESET_ALL
-        record.msg_color = self.colors.get(record.levelname, "")
-
-        return super().format(record)
-
+        return {"footfall": current_footfall}
