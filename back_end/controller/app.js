776d78d8c96b612954d9903fdb172e5814070b95
Jerick Cheong <jerickcheong@gmail.com>
2021-11-14 09:54:07 +0800

    Revert "try to fix serving index.html"
    This reverts commit 0cea61c30a4990c64a4de463c17fa65f6ec9bec0.

--- back_end/controller/app.js
+++ back_end/controller/app.js
@@ -70,9 +70,4 @@ app.post("/history", (req, res) => {
 // Serve static files from the frontend app
 app.use(express.static(path.join(__dirname, '../../front_end')))
 
-// AFTER defining routes: Anything that doesn't match what's above, send back index.html; (the beginning slash ('/') in the string is important!)
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/../../front_end/index.html'))
-})
-
 module.exports = app;
