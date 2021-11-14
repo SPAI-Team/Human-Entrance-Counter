39454678f3f27d2b94593e6b870ea0b09ba635b9
kiritowu <70313929+kiritowu@users.noreply.github.com>
2021-11-14 08:40:17 +0800

    Attempt to deploy front-end

--- front_end/index.js
+++ front_end/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/front_end'));
+
 app.listen(process.env.PORT || 5000, () => {
     console.log(`Client server has started listening on port ${process.env.PORT}`);
 });
